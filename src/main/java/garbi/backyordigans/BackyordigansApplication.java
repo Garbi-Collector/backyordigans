5e1a30a544e5f483ae43182f420e32dabefb4f84
Garbi-Collector <uwurulos@gmail.com>
2025-10-25 22:04:12 -0300

    init

---
+++ src/main/java/garbi/backyordigans/BackyordigansApplication.java
@@ -0,0 +1,13 @@
+package garbi.backyordigans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackyordigansApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackyordigansApplication.class, args);
+    }
+
+}
---
+++ src/test/java/garbi/backyordigans/BackyordigansApplicationTests.java
@@ -0,0 +1,13 @@
+package garbi.backyordigans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackyordigansApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
